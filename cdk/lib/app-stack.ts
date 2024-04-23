999ce991c1656ff795529de6a6e8154caae13794
sonuabraham-aremedia <124004596+sonuabraham-aremedia@users.noreply.github.com>
2024-04-23 14:04:00 +1000

    Update app-stack.ts1

--- cdk/lib/app-stack.ts
+++ cdk/lib/app-stack.ts
@@ -21,7 +21,7 @@ export class AppStack extends cdk.Stack {
       memoryLimitMiB: 512,
       cpu: 256,
     });
-
+/*
     // Add app container
     const appLogging = new ecs.AwsLogDriver({
       streamPrefix: "app",
@@ -35,7 +35,7 @@ export class AppStack extends cdk.Stack {
       logging: appLogging,
     });
     appContainer.addPortMappings({ containerPort: 3000 });
-
+*/
     // Add nginx container
     const nginxLogging = new ecs.AwsLogDriver({
       streamPrefix: "nginx",
